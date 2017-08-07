bde204d41f66f4d2816556892e52b6175418f79f
Pilar Barbero <pilar.barbero@digitaltakers.com>
2017-08-07 20:09:51 +0200

    fix hrefs

--- js/main.js
+++ js/main.js
@@ -18,13 +18,13 @@ $(document).ready( function() {
 	});	
 function animMeter(){
     $(".meter > span").each(function() {
-                $(this)
-                    .data("origWidth", $(this).width())
-                    .width(0)
-                    .animate({
-                        width: $(this).data("origWidth")
-                    }, 1200);
-            });
+    $(this)
+        .data("origWidth", $(this).width())
+        .width(0)
+        .animate({
+            width: $(this).data("origWidth")
+        }, 1200);
+});
 }
 animMeter();
 
@@ -35,7 +35,7 @@ animMeter();
         transitionOut	: 'slideUp',
         animationSpeed	: 600,
         tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
-            if($targetPanel.selector=='#resume'){
+            if($targetPanel.selector=='#work'){
                     animMeter();
             }
         });
